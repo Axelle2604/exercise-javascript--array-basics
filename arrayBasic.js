a2a535c50b6a06c3eb8c14dcfaab877cb1cda3ef
Clemo2604 <axelle.dru@stackerine.com>
2018-12-12 12:28:16 +0100

    exo

---
+++ arrayBasic.js
@@ -0,0 +1,53 @@
+const shoesList = ['Nike', 'Puma', 'Adidas', 'Jordans', 'Timberland'];
+
+console.log(shoesList);
+
+shoesList.unshift('nouvelleMarque');
+
+console.log(shoesList);
+console.log(shoesList.length);
+
+shoesList.shift();
+
+console.log(shoesList);
+console.log(shoesList.length);
+
+shoesList.push('nouvelleMarque');
+
+console.log(shoesList);
+console.log(shoesList.length);
+
+shoesList.pop();
+
+console.log(shoesList);
+console.log(shoesList.length);
+
+const shoesList2 = shoesList.slice(0, 3);
+
+console.log(shoesList2);
+console.log(shoesList2.length);
+
+const newShoesList = ['marque1', 'marque2', 'marque3', 'marque4', 'marque5'];
+
+const allShoes = shoesList2.concat(newShoesList);
+
+console.log(allShoes);
+console.log(allShoes.length);
+
+console.log(
+  'Marques contenant 4 lettres : ' +
+    allShoes.filter(function(mark) {
+      return mark.length === 4;
+    })
+);
+
+console.log(
+  'Marques contenant 4 lettres : ' +
+    allShoes.find(function(mark) {
+      return mark.length === 4;
+    })
+);
+
+console.log(
+  'Filter va sélectionner tous les éléments retournant true, tandis que find ne retournera que le premier retournant true.'
+);
